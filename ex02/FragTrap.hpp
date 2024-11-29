ddd60d86e25be4e83479cea8843bd760a593462b
Bovic101 <Victorbolanle72@gmail.com>
2024-11-29 20:57:14 +0100

    correction to the header file

--- ex02/FragTrap.hpp
+++ ex02/FragTrap.hpp
@@ -6,7 +6,25 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 20:43:09 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/29 20:43:10 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/29 20:56:25 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FRAGTRAP_HPP
+#define FRAGTRAP_HPP
+
+#include " ClapTrap.hpp"
+
+class FragTrap : public ClapTrap
+{
+	public:
+	FragTrap();
+	FragTrap(const std::string& Name);
+	FragTrap(const FragTrap& cp);
+	FragTrap& operator=(const FragTrap& cp);
+	~FragTrap();
+	
+	void highFivesGuys(void);
+};
+
+#endif
