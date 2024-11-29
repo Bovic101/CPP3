f717d5eee3f1692a36de446494ff599e5ca3e766
Bovic101 <Victorbolanle72@gmail.com>
2024-11-29 21:16:45 +0100

    Implementation of the main function and adjustment to makefile

--- ex02/FragTrap.cpp
+++ ex02/FragTrap.cpp
@@ -6,25 +6,50 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 20:43:30 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/29 20:52:11 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/29 21:08:34 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FRAGTRAP_HPP
-#define FRAGTRAP_HPP
+#include "FragTrap.hpp"
 
-#include " ClapTrap.hpp"
-
-class FragTrap : public ClapTrap
+// Default constructor
+FragTrap::FragTrap() : ClapTrap() 
 {
-	public:
-	FragTrap();
-	FragTrap(const std::string& Name);
-	FragTrap(const FragTrap& cp);
-	FragTrap& operator=(const FragTrap& cp);
-	~FragTrap();
-	
-	void highFivesGuys(void);
-};
+    _hitpoint = 100;
+    _energypoint = 100;
+    _attackDamage = 30;
+    std::cout << "FragTrap: Default constructor for " << _Name  << std::endl; // Construtor for ClapTrap is called first
+}
 
-#endif
+FragTrap::FragTrap(const std::string& cp) : ClapTrap(cp) 
+{
+    _hitpoint = 100;
+    _energypoint = 100;
+    _attackDamage = 30;
+    std::cout << "FragTrap: Intialised constructor for " << _Name << std::endl;  // Construtor for ClapTrap is called first
+}
+
+// Copy constructor
+FragTrap::FragTrap(const FragTrap& cp) : ClapTrap(cp) 
+{
+    std::cout << "FragTrap: This is a opy constructor for " << _Name << std::endl;
+}
+
+FragTrap& FragTrap::operator=(const FragTrap& cp) 
+{
+    if (this != &cp) {
+        ClapTrap::operator=(cp); // first call ClapTrap assignment operator
+    }
+    std::cout << "FragTrap: This is an ssignment operator for " << _Name << std::endl;
+    return *this;
+}
+
+FragTrap::~FragTrap() 
+{
+    std::cout << "FragTrap: Destroyed " << _Name << std::endl;
+}
+
+void FragTrap::highFivesGuys() 
+{
+    std::cout << "FragTrap: " << _Name << " highfiveguys is active" << std::endl;
+}
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,21 +6,15 @@
 /*   By: vodebunm <vodebunm@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/29 18:45:21 by vodebunm          #+#    #+#             */
-/*   Updated: 2024/11/29 19:10:48 by vodebunm         ###   ########.fr       */
+/*   Updated: 2024/11/29 21:12:52 by vodebunm         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include " ClapTrap.hpp"
+#include "FragTrap.hpp"
 
 int main()
 {
-	ClapTrap s1("Victor Warrior"); //passing parameter to constutor
-	ClapTrap s2= s1; //copy constr.
-	ClapTrap s3; //default constr.
-	
-	s1.attack("First enemy");
-	s1.takeDamage(4);
-	s1.beRepaired(6);
-	
+	FragTrap s1("FragTest");
+	s1.highFivesGuys();
 	return(0);
 }
